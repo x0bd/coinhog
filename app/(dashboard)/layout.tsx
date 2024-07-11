a6a60b66a4d36d9c0fabb4fabd598fd7818129b3
Tinodaishe Tembo <synthmancer@gmail.com>
2024-07-11 22:44:51 +0200

    fix navbar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,13 @@
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/NavBar";
 import React, { ReactNode } from "react";
 
 function layout({ children }: { children: ReactNode }) {
-  return (
-    <div className="relative flex h-screen w-full flex-col">
-      <Navbar />
-      <div className="w-full">{children}</div>
-    </div>
-  );
+	return (
+		<div className="relative flex h-screen w-full flex-col">
+			<Navbar />
+			<div className="w-full">{children}</div>
+		</div>
+	);
 }
 
 export default layout;
